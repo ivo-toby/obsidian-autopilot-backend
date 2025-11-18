05cb1a3d8363aaedcebfdb67a54929d1d724e3fe
Ivo Toby <ivo.toby@contentful.com>
2025-11-18 17:54:37 +0100

    chore: move away from pyenv

--- services/vector_store/chunking_service.py
+++ services/vector_store/chunking_service.py
@@ -3,7 +3,7 @@
 import logging
 from typing import Any, Dict, List
 
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 logger = logging.getLogger(__name__)
 
